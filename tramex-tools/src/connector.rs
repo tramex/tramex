d712d06361679a1ee4880a8c25ce9443a0c115cd
n4n5 <its.just.n4n5@gmail.com>
2024-06-11 19:57:11 +0200

    add

--- tramex-tools/src/connector.rs
+++ tramex-tools/src/connector.rs
@@ -26,9 +26,6 @@ pub struct Connector {
 
     /// Asking size max
     pub asking_size_max: u64,
-
-    /// Url
-    pub url: String,
 }
 
 impl Connector {
@@ -39,7 +36,6 @@ impl Connector {
             data: Data::default(),
             available: false,
             asking_size_max: 1024,
-            url: "ws://127.0.0.1:9001".to_owned(),
         }
     }
 
@@ -127,7 +123,7 @@ impl Connector {
         match &mut self.interface {
             Some(inter) => inter.get_more_data(_layers_list, self.asking_size_max, &mut self.data, &mut self.available),
             None => {
-                log::debug!("Error: Interface not set");
+                log::debug!("Error: get_more_data() Interface not set");
                 Ok(())
             }
         }
@@ -140,7 +136,7 @@ impl Connector {
         match &mut self.interface {
             Some(inter) => inter.try_recv(&mut self.data, &mut self.available),
             None => {
-                log::debug!("Error: Interface not set");
+                log::debug!("Error: try_recv() Interface not set");
                 Ok(())
             }
         }
